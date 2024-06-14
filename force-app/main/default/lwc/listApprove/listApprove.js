2d7be5cc211f92623b6e9a823cf051884ff1efa1
shigeo.ishii <shigeo.ishii@citron-reiwa.com>
2024-06-14 18:20:11 +0900

    修正

--- force-app/main/default/lwc/listApprove/listApprove.js
+++ force-app/main/default/lwc/listApprove/listApprove.js
@@ -2,8 +2,7 @@ import { LightningElement, wire} from 'lwc';
 import getApproveData from '@salesforce/apex/getData.GetApproveData';
 
 const actions = [
-    { label: 'Show details', name: 'show_details' },
-    { label: 'Delete', name: 'delete' },
+    { label: '申請', name: 'approve' },
 ];
 
 const columns = [
@@ -34,27 +33,29 @@ export default class DatatableWithRowActions extends LightningElement {
     //     this.data = generateData({ amountOfRecords: 100 });
     // }
 
+//    @api minBirthDate;
     handleRowAction(event) {
         const actionName = event.detail.action.name;
         const row = event.detail.row;
         switch (actionName) {
-            case 'delete':
-                this.deleteRow(row);
-                break;
-            case 'show_details':
-                this.showRowDetails(row);
+            case 'approve':
+                // TODO:申請処理
+                approveUpdate({ 
+                    // 引数として申請対象となるID
+
+                })
+                .then(strRet => {
+                    // 申請完了したらDataTable更新
+                    this.data = this.data
+                        .slice(0, this.findRowIndexById(row.id))
+                        .concat(this.data.slice(this.findRowIndexById(row.id) + 1));
+
+                })
+                .catch(error => {
+                });
+        
                 break;
             default:
-        }
-    }
-
-    deleteRow(row) {
-        const { id } = row;
-        const index = this.findRowIndexById(id);
-        if (index !== -1) {
-            this.data = this.data
-                .slice(0, index)
-                .concat(this.data.slice(index + 1));
         }
     }
 
@@ -70,7 +71,5 @@ export default class DatatableWithRowActions extends LightningElement {
         return ret;
     }
 
-    showRowDetails(row) {
-        this.record = row;
-    }
+
 }
